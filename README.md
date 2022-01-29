47aee96a2487de8d1b8ff7eac80df175528d90c9
Nirutt <niruttsibutda@gmail.com>
2022-01-29 23:48:05 +0000

    First commit

---
+++ README.md
@@ -0,0 +1,25 @@
+# Linktree-like web application
+
+👍 This web application is my take on the social-media-click-through 
+websites that's becoming quite common in affluent channels. An
+example of this can be found at [linktree](https://linktr.ee/) and
+flavours on almost even instagram user's bio.
+
+### Development stages
+1. Design UI/UX prototype ✅ (Completed)
+2. Implement HTML and CSS ✅ (Completed)
+3. Backend to enable SaaS 🎯 (In progress)
+
+The goal for this application is to have a dynamic front page 
+which can be seen by anyone that clicks through from any available
+social media platforms. 
+
+The front page would consist of buttons with its data loaded 
+from a database. There will also be a private admin page with
+functions to add, edit and remove buttons which would directly
+affect the buttons shown at the front page.
+
+👑 Overall this will enable the admin (for this instance, me) to
+efficiently add, edit or remove buttons without the need to
+hard code extra buttons in the HTML file.
+
---
+++ nirutt.php
@@ -0,0 +1,50 @@
+<?php
+    header("Cache-Control: max-age=31536001");
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" href="css/nirutt.css?v=0.2">
+    <!-- Meta information -->
+    <meta name="title" content="Nirutt Anstee Linktree">
+    <meta name="description" content="A linktree website with buttons directing users to information about Nirutt Anstee.">
+    <meta name="keywords" content="Linktree, link page, Nirutt Anstee">
+    <meta name="robots" content="index, follow">
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+    <meta name="language" content="English">
+    <title>Nirutt Anstee's Linktree</title>
+    <!-- import Google font -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Lato:ital,wght@0,100;0,300;0,400;0,700;0,900;1,100;1,300;1,400;1,700;1,900&display=swap" rel="stylesheet">
+    <!-- Favicon -->
+    <link rel="apple-touch-icon" sizes="180x180" href="/images/apple-touch-icon.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="/images/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="/images/favicon-16x16.png">
+    <link rel="manifest" href="/images/site.webmanifest">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0"></head>
+<body>
+    <div id="content-block">
+        <div id="top-container">
+            <img id="profile-picture" src="images/profile-picture-nirutt.png" alt="Memoji image of Nirutt Anstee">
+            <p class="profile-link">@niruttanstee</p>
+            <p class="motto">It's logical, said the Vulcan 🖖</p>
+        </div>
+        <div id="links-container">
+            <a href="https://github.com/niruttanstee?tab=overview&from=2022-01-01&to=2022-01-23" class="link-button">
+                <img class="button-image" src="images/white-github.svg" alt="">
+                <p class="button-text">Github</p>
+            </a>
+            <a href="https://github.com/nirutts/simple-linktree-web-application" class="link-button">
+                <img class="button-image" src="images/light-bulb_1f4a1.png" alt="An emoji of a light bulb.">
+                <p class="button-text">How I made this website</p>
+            </a>
+            <a href="https://github.com/nirutts/portfolio" class="link-button">
+                <img class="button-image" src="images/sign-of-the-horns_1f918.png" alt="An emoji of a rock sign using the hand.">
+                <p class="button-text">Portfolio website (Coming soon)</p>
+            </a>
+        </div>
+    </div>
+</body>
+</html>
